6c78aa9f6fe9b8e4d1e7b5ed6be4ca99dd9f01db
JP <AntonovichJP@gmail.com>
2018-09-29 21:31:46 -0700

    Added minor javascript events to the page

--- static/script.js
+++ static/script.js
@@ -1,4 +1,24 @@
 $(document).ready(() => {
+    $("h1").hover(function() {
+        $(this).css("text-decoration", "underline");
+        }, function() {
+            $(this).css("text-decoration", "none");
+    });
+    
+    $("h2").hover(function() {
+        $(this).css("text-decoration", "underline");
+        }, function() {
+            $(this).css("text-decoration", "none");
+    });
+    
+    
+    $("a").hover(function() {
+        $(this).css("font-weight", "Bold");
+        }, function(){
+        $(this).css("font-weight", "Normal");
+    });
+    
     const $thisDot = $('<div class="this-dot"><iframe width="850" height="500" src="https://www.youtube.com/embed/M5d7vygUPoQ?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>');
     $('p:last').after($thisDot);
+    
 });
